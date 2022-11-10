175cd6a8bd00c985611c6b235280f7f5bae816ff
zayats <zayats.a1@yandex.ru>
2022-11-10 14:39:00 +0300

    create navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import {
+  BrowserRouter, Link, Navigate, Route, Routes
+ } from 'react-router-dom';
 import './App.css';
 
 function App(): JSX.Element {
   return (
-    <div className="App" />
+    <BrowserRouter>
+    <nav className="nav">
+      <Link className="links" type="button" to="/">Главная</Link>
+      <Link className="links" to="/order">Сделать заказ</Link>
+      <Link className="links" to="/registration">Регистрация</Link>
+      <Link className="links" to="/login">Войти</Link>
+    </nav>
+    <Routes>
+    <Route path="/" element={<Navigate to="/clean_city" />} />
+      {/* <Route path="/registration" element={<Registration />} /> */}
+      {/* <Route path="/login" element={<Login />} /> */}
+    </Routes>
+    </BrowserRouter>
   );
 }
 
