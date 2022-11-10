8e20df96dd1fc5f85b75f34df0c1e70adaff771b
Cherry <choduraatyulyush@mail.ru>
2022-11-10 15:41:33 +0300

    render authPage

--- frontend/src/Authorization/Authorization.tsx
+++ frontend/src/Authorization/Authorization.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import { useForm } from 'react-hook-form';
+import React, { useState } from 'react';
 
-function Authorization(): JSX.Element {
+import './Authorization/styles.css';
+
+export default function Authorization(): JSX.Element {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { register, handleSubmit } = useForm();
+  const onSubmit = ():void => console.log('data');
+
+  function handleEmailChange(): void {
+    console.log('email');
+  }
+  function handlePasswordChange(): void {
+    console.log('password');
+  }
   return (
-    <h1>Hello</h1>
+     <form onSubmit={handleSubmit(onSubmit)}>
+      <h2>Authorization</h2>
+      <label htmlFor="email-input">Email</label>
+      <input type="text" id="email-input" value={email} onChange={handleEmailChange} />
+      <label htmlFor="password-input">Password</label>
+      <input type="text" id="password-input" value={password} onChange={handlePasswordChange} />
+
+      <button type="submit">Submit</button>
+     </form>
+
   );
 }
-
-export default Authorization;
