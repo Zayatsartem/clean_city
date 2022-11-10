1c2007ed58c8c3e6e000fbb91a3db154a13e9c9f
sonyalit <sonyalit@yandex.ru>
2022-11-10 20:00:25 +0300

    call form don without db

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import {
+ BrowserRouter, Link, Route, Routes
+} from 'react-router-dom';
 import './App.css';
+import CallForm from './Main/CallForm';
 
 function App(): JSX.Element {
   return (
-    <div className="App" />
+
+    <BrowserRouter>
+
+      <nav>
+        <Link to="/todos">Задачи</Link>
+
+      </nav>
+
+      <Routes>
+
+        <Route path="/todos" element={<CallForm />} />
+
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
