baccc623f0cbbcd34079bcbbf748127f847df40c
sonyalit <sonyalit@yandex.ru>
2022-11-12 14:18:57 +0300

    fixed admin in res locals

--- backend/db/seeders/20221110093806-demo-users.js
+++ backend/db/seeders/20221110093806-demo-users.js
@@ -11,7 +11,7 @@ module.exports = {
           email: 'test@mail.com',
           password: await bcrypt.hash('12345678', 10),
           telephone: '+79801234567',
-          admin: false,
+          admin: true,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
