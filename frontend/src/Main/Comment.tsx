4a9034ca0ed3977a69164d8bfcb5e0a46ca9c1ba
sonyalit <sonyalit@yandex.ru>
2022-11-14 13:06:29 +0300

    more fixes

--- frontend/src/Main/Comment.tsx
+++ frontend/src/Main/Comment.tsx
@@ -4,7 +4,6 @@ import { TComment } from '../Admin/Adminslice';
 
 function Comment({ comment }: { comment: TComment }): JSX.Element {
   return (
-    <div>
       <Card sx={{ minWidth: 275 }}>
         <CardContent>
           <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
@@ -18,7 +17,7 @@ function Comment({ comment }: { comment: TComment }): JSX.Element {
           </Typography>
         </CardContent>
       </Card>
-    </div>
+
   );
 }
 
