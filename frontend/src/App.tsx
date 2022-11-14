2c6a17af5a16cbe3cd11e7d6fab09cdd6bbaee51
Rushaniya <rusha359@gmail.com>
2022-11-14 22:58:00 +0300

    Services card

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,11 +7,13 @@ import './App.css';
 import Navbar from './Navbar/Navbar';
 
 import EditProfile from './Profile/EditProfile';
+import Services from './Services/Services';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Navbar />
+      <Services />
       <Routes>
         <Route path="/profile/edit" element={<EditProfile />} />
       </Routes>
