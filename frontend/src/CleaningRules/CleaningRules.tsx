82c873671881d82dd9b17254a4282f18d9d2d70d
zayats <zayats.a1@yandex.ru>
2022-11-15 15:59:42 +0300

    add grid

--- frontend/src/CleaningRules/CleaningRules.tsx
+++ frontend/src/CleaningRules/CleaningRules.tsx
@@ -1,9 +1,52 @@
-import React from 'react';
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
 
-function CleaningRules(): JSX.Element {
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+export default function CleaningRules(): JSX.Element {
   return (
-  <div className=''></div>
+    <div className="container">
+      <h3>Регламент уборки</h3>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={3}>
+          <Grid xs={4}>
+            <Item>Вся квартира</Item>
+          </Grid>
+          <Grid xs={4}>
+            <Item>Входит в стандартную уборку</Item>
+          </Grid>
+          <Grid xs={4}>
+            <Item>Можно добавить</Item>
+          </Grid>
+          <Grid xs={4}>
+            <Item>Кухня</Item>
+          </Grid>
+          <Grid xs={4}>
+            <Item>Входит в стандартную уборку</Item>
+          </Grid>
+          <Grid xs={4}>
+            <Item>Можно добавить</Item>
+          </Grid>
+          <Grid xs={4}>
+            <Item>Санузел</Item>
+          </Grid>
+          <Grid xs={4}>
+            <Item>Входит в стандартную уборку</Item>
+          </Grid>
+          <Grid xs={4}>
+            <Item>Можно добавить</Item>
+          </Grid>
+        </Grid>
+      </Box>
+    </div>
   );
 }
-
-export default CleaningRules;
