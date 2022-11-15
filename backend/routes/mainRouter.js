264a7873fe9fcfb9ac8c005b684a88493ac181ec
zayats <zayats.a1@yandex.ru>
2022-11-15 12:45:57 +0300

    fix bug

--- backend/routes/mainRouter.js
+++ backend/routes/mainRouter.js
@@ -17,25 +17,23 @@ router.get('/comments', async (req, res) => {
 
 router.post('/request', async (req, res) => {
   const br = '%0A';
-  const { CHAT_ID } = process.env;
-  const { BOT } = process.env;
+  const { CHAT_ID, BOT } = process.env;
   try {
     if (req.body.rooms && req.body.bathrooms) {
       const { rooms, bathrooms, phone } = req.body;
       const text = `${rooms} квартира ${br}${bathrooms} ${br}Телефон: ${phone}`;
       await fetch(
-        `https://api.telegram.org/bot${BOT}/sendMessage?chat_id=${CHAT_ID}&parse_mode=HTML&text=${text}`,
+        `https://api.telegram.org/bot${BOT}/sendMessage?chat_id=${CHAT_ID}&parse_mode=HTML&text=${text}`
       );
       res.json({
         message:
           'Ваша заявка принята. В ближайшее время с вами свяжется менеджер чтобы уточнить детали заказа',
       });
-    }
-    if (req.body.comment) {
+    } else {
       const { phone, comment } = req.body;
       const text = `Телефон: ${phone}${br}Комментарий: ${comment}`;
       await fetch(
-        `https://api.telegram.org/bot${BOT}/sendMessage?chat_id=${CHAT_ID}&parse_mode=HTML&text=${text}`,
+        `https://api.telegram.org/bot${BOT}/sendMessage?chat_id=${CHAT_ID}&parse_mode=HTML&text=${text}`
       );
       res.json({
         message:
