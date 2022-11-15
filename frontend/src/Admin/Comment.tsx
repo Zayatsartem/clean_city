3b7ebd4fa114ad660a5fb385e342cebcb8eae85c
sonyalit <sonyalit@yandex.ru>
2022-11-15 12:05:39 +0300

    move admin types to types folder

--- frontend/src/Admin/Comment.tsx
+++ frontend/src/Admin/Comment.tsx
@@ -1,14 +1,15 @@
 import { TableRow, TableCell, Button } from '@mui/material';
 import React from 'react';
 import { useAppDispatch } from '../store';
-import { deleteCommentForAll, publishComment, TComment } from './Adminslice';
+import { TComment } from '../types/AdminTypes';
+import { deleteCommentForAll, publishComment } from './Adminslice';
 
-function Comment({ comment }:{ comment:TComment }):JSX.Element {
+function Comment({ comment }: { comment: TComment }): JSX.Element {
   const dispatch = useAppDispatch();
-  function publish(id:number):void {
-   dispatch(publishComment(id));
+  function publish(id: number): void {
+    dispatch(publishComment(id));
   }
-  function deleteComment(id:number):void {
+  function deleteComment(id: number): void {
     dispatch(deleteCommentForAll(id));
   }
   return (
@@ -16,12 +17,12 @@ function Comment({ comment }:{ comment:TComment }):JSX.Element {
       key={comment.id}
       sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
     >
-        <TableCell component="th" scope="row">
-          {comment.name}
-        </TableCell>
-        <TableCell align="right">{comment.date}</TableCell>
-        <TableCell align="right">{comment.title}</TableCell>
-        <TableCell align="right"><Button variant="outlined" onClick={() => publish(comment.id)}>Опубликовать</Button><Button variant="outlined" onClick={() => deleteComment(comment.id)}>Удалить</Button></TableCell>
+      <TableCell component="th" scope="row">
+        {comment.name}
+      </TableCell>
+      <TableCell align="right">{comment.date}</TableCell>
+      <TableCell align="right">{comment.title}</TableCell>
+      <TableCell align="right"><Button variant="outlined" onClick={() => publish(comment.id)}>Опубликовать</Button><Button variant="outlined" onClick={() => deleteComment(comment.id)}>Удалить</Button></TableCell>
     </TableRow>
   );
 }
--- frontend/src/Admin/NewOrder.tsx
+++ frontend/src/Admin/NewOrder.tsx
@@ -3,31 +3,32 @@ import TableCell from '@mui/material/TableCell';
 import TableRow from '@mui/material/TableRow';
 import React from 'react';
 import { useAppDispatch } from '../store';
-import { addWIPOrder, cancelNewOrder, TNewOrder } from './Adminslice';
+import { TNewOrder } from '../types/AdminTypes';
+import { addWIPOrder, cancelNewOrder } from './Adminslice';
 
-function NewOrder({ order }:{ order:TNewOrder }):JSX.Element {
+function NewOrder({ order }: { order: TNewOrder }): JSX.Element {
   const dispatch = useAppDispatch();
-  function addWIP(id:number):void {
-   dispatch(addWIPOrder(id));
+  function addWIP(id: number): void {
+    dispatch(addWIPOrder(id));
   }
-  function cancel(id:number):void {
+  function cancel(id: number): void {
     dispatch(cancelNewOrder(id));
   }
   return (
-      <TableRow
-        key={order.id}
-        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-      >
-        <TableCell component="th" scope="row">
-          {order.name}
-        </TableCell>
-        <TableCell align="right">{order.telephone}</TableCell>
-        <TableCell align="right">{order.date}</TableCell>
-        <TableCell align="right">{order.time}</TableCell>
-        <TableCell align="right">{order.address}</TableCell>
-        <TableCell align="right">Комнат:{order.rooms}, санузлов:{order.bathrooms}</TableCell>
-        <TableCell align="right"><Button variant="outlined" onClick={() => addWIP(order.id)}>Принять в работу</Button><Button variant="outlined" onClick={() => cancel(order.id)}>Отменить</Button></TableCell>
-      </TableRow>
+    <TableRow
+      key={order.id}
+      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+    >
+      <TableCell component="th" scope="row">
+        {order.name}
+      </TableCell>
+      <TableCell align="right">{order.telephone}</TableCell>
+      <TableCell align="right">{order.date}</TableCell>
+      <TableCell align="right">{order.time}</TableCell>
+      <TableCell align="right">{order.address}</TableCell>
+      <TableCell align="right">Комнат:{order.rooms}, санузлов:{order.bathrooms}</TableCell>
+      <TableCell align="right"><Button variant="outlined" onClick={() => addWIP(order.id)}>Принять в работу</Button><Button variant="outlined" onClick={() => cancel(order.id)}>Отменить</Button></TableCell>
+    </TableRow>
 
   );
 }
