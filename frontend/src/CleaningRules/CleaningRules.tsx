6960f2e219ab628ff674385d6b8e25075a64097d
zayats <zayats.a1@yandex.ru>
2022-11-15 18:07:27 +0300

    add img and style

--- frontend/src/CleaningRules/CleaningRules.tsx
+++ frontend/src/CleaningRules/CleaningRules.tsx
@@ -3,6 +3,11 @@ import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Unstable_Grid2';
+import check from './images/check.png';
+import kitchen from './images/kitchen.jpg';
+import flat from './images/flat.jpg';
+import bathroom from './images/bathroom.jpg';
+import './cleaningrules.css';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -19,25 +24,80 @@ export default function CleaningRules(): JSX.Element {
       <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={3}>
           <Grid xs={4}>
-            <Item>Вся квартира</Item>
+            <Item>
+              <div className="imgItemBlock">
+                <h3>Вся квартира</h3>
+                <img src={flat} alt="flat" className="serviceImg" />
+              </div>
+            </Item>
           </Grid>
           <Grid xs={4}>
-            <Item>Входит в стандартную уборку</Item>
+            <Item>
+              <span>Входит в стандартную уборку</span>
+              <div className="serviceList">
+                <div className="service">
+                  <img src={check} alt="check" />
+                  <span>Влажная уборка </span>
+                </div>
+                <div className="service">
+                  <img src={check} alt="check" />
+                  <span>Влажная уборка </span>
+                </div>
+                <div className="service">
+                  <img src={check} alt="check" />
+                  <span>Влажная уборка </span>
+                </div>
+                <div className="service">
+                  <img src={check} alt="check" />
+                  <span>Влажная уборка </span>
+                </div>
+              </div>
+            </Item>
           </Grid>
           <Grid xs={4}>
             <Item>Можно добавить</Item>
           </Grid>
           <Grid xs={4}>
-            <Item>Кухня</Item>
+            <Item>
+              <div className="imgItemBlock">
+                <h3>Кухня</h3>
+                <img src={kitchen} alt="kitchen" className="serviceImg" />
+              </div>
+            </Item>
           </Grid>
           <Grid xs={4}>
-            <Item>Входит в стандартную уборку</Item>
+            <Item>
+              <span>Входит в стандартную уборку</span>
+              <div className="serviceList">
+                <div className="service">
+                  <img src={check} alt="check" />
+                  <span>Влажная уборка </span>
+                </div>
+                <div className="service">
+                  <img src={check} alt="check" />
+                  <span>Влажная уборка </span>
+                </div>
+                <div className="service">
+                  <img src={check} alt="check" />
+                  <span>Влажная уборка </span>
+                </div>
+                <div className="service">
+                  <img src={check} alt="check" />
+                  <span>Влажная уборка </span>
+                </div>
+              </div>
+            </Item>
           </Grid>
           <Grid xs={4}>
             <Item>Можно добавить</Item>
           </Grid>
           <Grid xs={4}>
-            <Item>Санузел</Item>
+            <Item>
+              <div className="imgItemBlock">
+                <h3>Санузел</h3>
+                <img src={bathroom} alt="bathroom" className="serviceImg" />
+              </div>
+            </Item>
           </Grid>
           <Grid xs={4}>
             <Item>Входит в стандартную уборку</Item>
