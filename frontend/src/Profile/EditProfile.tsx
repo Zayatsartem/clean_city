c76c1f1eeb8cce7e386c74c09cc8791e5ef3944a
Rushaniya <rusha359@gmail.com>
2022-11-16 12:44:03 +0300

    css in edit profile

--- frontend/src/Profile/EditProfile.tsx
+++ frontend/src/Profile/EditProfile.tsx
@@ -3,6 +3,8 @@ import { useSelector } from 'react-redux';
 // import { useNavigate } from 'react-router-dom';
 import { RootState, useAppDispatch } from '../store';
 import { editProfile } from './editingSlice';
+import './styles.css';
+import avatar from './avatar.jpg';
 
 function EditProfile(): JSX.Element {
   const user = useSelector((state: RootState) => state.auth.user);
@@ -45,26 +47,61 @@ function EditProfile(): JSX.Element {
       })
     );
 
-  //   if (editProfile.fulfilled.match(dispatchEdit)) {
-  //   }
+    //   if (editProfile.fulfilled.match(dispatchEdit)) {
+    //   }
   }
 
   return (
-    <div className="form-edit">
-    {user ? (
-      <>
-      <form onSubmit={editForm}>
-      <h1 className="h1">{name}, Вы можете изменить профиль</h1>
-      <input type="text" value={name} onChange={changeName} placeholder="new name" />
-      <input type="text" value={telephone} onChange={changeTelephone} placeholder="new phone" />
-      <input type="text" value={email} onChange={changeEmail} placeholder="new email" />
-      <button type="submit">Изменить</button>
-      </form>
-      <div>{error && <p>{error}</p>}</div>
-      </>
-) : (
-      <h3>Чтобы изменить профиль, войдите в систему</h3>
-    )}
+    <div className="container">
+      <div className="form-edit box">
+        <img src={avatar} alt="avatar" />
+        {user ? (
+          <>
+            <form onSubmit={editForm} className="js-form">
+              <h1 className="h1">{name}, Вы можете изменить профиль</h1>
+              <label className="cc-input">
+                {' '}
+                Имя
+                <input
+                  type="text"
+                  value={name}
+                  onChange={changeName}
+                  placeholder="new name"
+                  className="cc-input"
+                />
+              </label>{' '}
+              <label className="cc-input">
+                {' '}
+                телефон
+                <input
+                  type="text"
+                  value={telephone}
+                  onChange={changeTelephone}
+                  placeholder="new phone"
+                  className="cc-input"
+                />
+              </label>
+              <label className="cc-input">
+                {' '}
+                email
+                <input
+                  type="text"
+                  value={email}
+                  onChange={changeEmail}
+                  placeholder="new email"
+                  className="cc-input "
+                />
+              </label>
+              <button type="submit" className="cc-submitButton">
+                Изменить
+              </button>
+            </form>
+            <div>{error && <p>{error}</p>}</div>
+          </>
+        ) : (
+          <h3>Чтобы изменить профиль, войдите в систему</h3>
+        )}
+      </div>
     </div>
   );
 }
