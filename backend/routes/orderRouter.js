6aa3e52a82a0801638850bb6e42ee98851279707
Rushaniya <rusha359@gmail.com>
2022-11-16 21:58:37 +0300

    calculation of the order price

--- backend/routes/orderRouter.js
+++ backend/routes/orderRouter.js
@@ -1,14 +1,27 @@
 const router = require('express').Router();
 
-const { Order } = require('../db/models');
+const { Order, Service } = require('../db/models');
 
 router.post('/', async (req, res) => {
-  const { rooms, bathrooms, date, time, address, user_id } = req.body;
-  console.log(req.body);
+  const { rooms, bathrooms, date, time, address, user_id, checkbox } = req.body;
+
   if (!rooms || !bathrooms || !date || !time || !address) {
     res.status(422).json({ error: 'поле не должно быть пустым' });
     return;
   }
+
+  const services = [];
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < checkbox.length; i++) {
+    // eslint-disable-next-line no-await-in-loop
+    const service = await Service.findOne({ where: { id: Number(checkbox[i]) } });
+    services.push(service);
+  }
+
+  const priceServices = services.map((el) => el.price).reduce((x, y) => x + y, 0);
+  const totalPrice = rooms * 1000 + bathrooms * 1000 + priceServices;
+  const servicesTitle = services.map((el) => el.title);
+  console.log(totalPrice);
 
   if (req.session.userId === user_id) {
     try {
@@ -16,8 +29,8 @@ router.post('/', async (req, res) => {
         user_id: req.session.userId,
         rooms: Number(rooms),
         bathrooms: Number(bathrooms),
-        price: null,
-        services: '',
+        price: totalPrice,
+        services: servicesTitle.join(', '),
         date,
         time,
         status: 'new',
